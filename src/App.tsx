6dd7fda95c2479768f0652edff8a1b1d9d91a301
nithyaprakash-t <nithyaprakash.t@gmail.com>
2024-03-31 14:13:28 +0530

    added sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Airroutes from './graphs/airport/Airroutes';
 
 function App() {
   return (
     <>
-    <header></header>
+    {/* <header></header> */}
     <main className='main'>
-      <div className='app-main'>
-        <Airroutes></Airroutes>
-      </div>
+      <Router>
+        <div className='app-main'>
+          <div className='app-main-left'>
+            <nav className='app-sidebar'>
+              <ul className='app-sidebar-list'>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/graphs-traversal">BFS, DFS</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          
+          <div className='app-main-right'>
+            <Routes>
+              <Route
+                  path="/"
+                  element={null}
+              ></Route>
+              <Route
+                  path="/graphs-traversal"
+                  element={<Airroutes />}
+              ></Route>
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </main>
-    <footer></footer>
+    {/* <footer></footer> */}
     </>
   );
 }
